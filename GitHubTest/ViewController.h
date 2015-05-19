a943851d4f491fee0844abde521bf05ac5bd3405
Nikola Bozinovic <nikolabozinovic@Nikolas-iMac.local>
2015-05-19 09:06:10 +0200

    Initial Commit

---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by Nikola Bozinovic on 5/19/15.
+//  Copyright (c) 2015 Nikola Bozinovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHubTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by Nikola Bozinovic on 5/19/15.
+//  Copyright (c) 2015 Nikola Bozinovic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
